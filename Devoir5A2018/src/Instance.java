d025708028ef2532338f52690670e05e27f423df
= <=>
2018-11-24 17:58:18 +0100

    modif

--- Devoir5A2018/src/Instance.java
+++ Devoir5A2018/src/Instance.java
@@ -1,16 +1,18 @@
+
 import java.util.Random;
+
 
 public class Instance {
 
 	private double[][] matriceAdj;
 	private Integer[] grapheVille;
-	private final int MAX = 1000;
+	private final int MAX = 9000;
 	private final double PAS_DE_CHEMIN = Double.POSITIVE_INFINITY;
 	
 	public Instance()
 	{
 		Random random = new Random();
-		int taille = MAX;
+		int taille = random.nextInt(MAX)+2;
 		matriceAdj = new double [taille][taille];
 		grapheVille = new Integer[taille];
 		
@@ -26,13 +28,13 @@ public class Instance {
 				{
 					matriceAdj[ligne][colonne] = random.nextInt(10);
 					matriceAdj[colonne][ligne] = matriceAdj[ligne][colonne];
-					/*int aléatoire = random.nextInt(10);
+					int aléatoire = random.nextInt(10);
 					
 					if (aléatoire == 0)
 					{
 						matriceAdj[colonne][ligne] = PAS_DE_CHEMIN;
 						matriceAdj[ligne][colonne] = PAS_DE_CHEMIN;
-					}*/
+					}
 				}
 			}
 			grapheVille[ligne] = ligne;
@@ -70,23 +72,73 @@ public class Instance {
 	
 	public static void main(String[] args)
 	{
-		Instance instance = new Instance();
-		/*System.out.println(instance.toString());
-		System.out.println(instance.getGrapheVille().length);*/
-		if (instance.getGrapheVille().length < 12)
+		int n = 10;
+		int i = 0;
+		Solution solutionF = new Solution();
+		
+		System.out.println("LocalSearch \t Génétique \t ForceBrute");
+		for (int boucle = 0; boucle < 10; boucle ++)
 		{
-			ForceBrute forceBrute = new ForceBrute(instance);
-			Solution solutionF = forceBrute.forceBrute();
-			System.out.println(solutionF.toString());
+			Instance instance = new Instance();
+			System.out.println("nombre de ville : "+instance.getGrapheVille().length);
+			/*System.out.println(instance.toString());
+			System.out.println(instance.getGrapheVille().length);*/
+			
+			long startL = System.nanoTime();
+			LocalSearch localSearch = new LocalSearch(instance);
+			Solution solutionL = localSearch.localSearch();
+			long endL = System.nanoTime();
+			long traitementL = endL - startL;
+			startL = System.nanoTime();
+			i = 0;
+			
+			while(i < n)
+			{
+				i++;
+			}
+			endL = System.nanoTime();
+			long servitudeL = startL - endL;
+			long tempL = (traitementL-servitudeL)/n;
+			//System.out.println(solutionL.toString());
+			
+			long startG = System.nanoTime();
+			Genetique genetique = new Genetique(instance);
+			Solution solutionG = genetique.algoGenetique(2, 50, 70);
+			long endG = System.nanoTime();
+			long traitementG = endG - startG;
+			startG = System.nanoTime();
+			i = 0;
+			
+			while(i < n)
+			{
+				i++;
+			}
+			endG = System.nanoTime();
+			long servitudeG = startG - endG;
+			long tempG = (traitementG-servitudeG)/n;
+			//System.out.println(solutionG.toString());
+			long tempF = 0;
+			if (instance.getGrapheVille().length <= 12)
+			{
+				long startF = System.nanoTime();
+				ForceBrute forceBrute = new ForceBrute(instance);
+				solutionF = forceBrute.forceBrute();
+				long endF = System.nanoTime();
+				long traitementF = endF-startF;
+				startF = System.nanoTime();
+				i = 0;
+				while(i < n)
+				{
+					i++;
+				}
+				endF = System.nanoTime();
+				long servitudeF = startF - endF;
+				tempF = (traitementF-servitudeF)/n;
+			}
+			System.out.println("Distance : "+ solutionL.getDistance() + " \t " + solutionG.getDistance() + " \t " + solutionF.getDistance());
+			System.out.println("Temps : "+ tempL + " \t " + tempG + " \t " + tempF);
 		}
-		System.out.println("ici");
-		LocalSearch localSearch = new LocalSearch(instance);
-		Solution solutionL = localSearch.localSearch();
-		//System.out.println(solutionL.toString());
-		System.out.println("ici deux");
-		Genetique genetique = new Genetique(instance);
-		Solution solutionG = genetique.algoGenetique(4, 100, 40);
-		//System.out.println(solutionG.toString());
+		
 	}
 	
 	
--- Devoir5A2018/src/LocalSearch.java
+++ Devoir5A2018/src/LocalSearch.java
@@ -1,4 +1,5 @@
 
+
 public class LocalSearch {
 	private Solution initialSolution;
 	
@@ -10,19 +11,31 @@ public class LocalSearch {
 	public Solution localSearch(){
         // Create some basics solutions
     	Solution bestNeighbourSolution = initialSolution.clone();
-               
-        // Compare the objectives
-        for (int i = 0; i < (initialSolution.getGrapheVille().length); i++) {
-            for (int j = i+1; j < (initialSolution.getGrapheVille().length); j++) {
-            	Solution neightbourSolution = initialSolution.clone();
-            	neightbourSolution.setGrapheVille(neightbourSolution.swap(i,j));;
-                neightbourSolution.cout();
+    	Solution bestSolution = initialSolution.clone();
+        boolean continu = true;
+        
+    	while (continu) {
+			// Compare the objectives
+    		for (int i = 0; i < (initialSolution.getGrapheVille().length); i++) {
+    			for (int j = i+1; j < (initialSolution.getGrapheVille().length); j++) {
+    				Solution neightbourSolution = initialSolution.clone();
+    				neightbourSolution.setGrapheVille(neightbourSolution.swap(i,j));;
+    				neightbourSolution.cout();
 
-                if (neightbourSolution.getDistance() < bestNeighbourSolution.getDistance()) {
-                    bestNeighbourSolution = neightbourSolution.clone();
-                }
-            }
-        }
-        return bestNeighbourSolution;
-    }
+    				if (neightbourSolution.getDistance() < bestNeighbourSolution.getDistance()) {
+    					bestNeighbourSolution = neightbourSolution.clone();
+    				}
+    			}
+    		}
+    		if (bestNeighbourSolution.getDistance() < bestSolution.getDistance())
+    		{
+    			bestSolution = bestNeighbourSolution.clone();
+    		}
+    		else
+    		{
+    			continu = false;
+    		}
+    	}
+    	return bestSolution;   
+	}
 }
